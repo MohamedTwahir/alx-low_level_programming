bb697aec742fb6aea43d7eeb648306aa36b66a85
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-02-21 10:25:07 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet - Begin Here
+ *
+ *Print a-z
+ */
+
+void print_alphabet(void)
+{
+    char ch = 'a';
+    while (ch <= 'z')
+    {
+        putchar(ch);
+        ch++;
+    }
+    putchar('\n');
+}
