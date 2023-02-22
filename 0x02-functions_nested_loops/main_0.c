bb471834d1085ea10dabb673a766dc8b76bf8376
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-02-22 11:42:49 -0800

    main_0.c

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * test_islower - Test the _islower function
+ * 'test_islower' - Test the _islower function
  *
  * @n: Number to pass to _islower function
  */
@@ -11,8 +11,8 @@ void test_islower(int n)
 	int r;
 
 	r = _islower(n);
-	_putchar(r + '0');
-	_putchar('\n');
+	putchar(r + '0');
+	putchar('\n');
 }
 
 /**
