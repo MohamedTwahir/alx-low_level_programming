d5fa75554d9779183d6b3718363f9375f71661ca
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-02-23 05:21:20 -0800

    Numbers have life

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,35 +1,30 @@
-#include<stdio.h>
 #include "main.h"
+
+/**
+ * print_numbers - prints an integer
+ * @num: the integer to print
+ */
+void print_numbers(int num)
+{
+    /* function body */
+}
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
 int main(void)
-  
 {
-  
-  print_numbers(98);
-  
-  _putchar('\n');
-  
-  print_numbers(402);
-  
-  _putchar('\n');
-  
-  print_numbers(1024);
-  
-  _putchar('\n');
-  
-  print_numbers(0);
-  
-  _putchar('\n');
-  
-  print_numbers(-98);
-  
-  _putchar('\n');
-  
-  return (0);
-  
+print_numbers(98);
+_putchar('\n');
+print_numbers(402);
+_putchar('\n');
+print_numbers(1024);
+_putchar('\n');
+print_numbers(0);
+_putchar('\n');
+print_numbers(-98);
+_putchar('\n');
+return (0);
 }
